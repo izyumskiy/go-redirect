eeb2ae85a0695a105e4f7e4356d5d4d72c092fa8
Natalia <Natalia>
2016-06-07 09:31:05 +0300

    base files

---
+++ main.go
@@ -0,0 +1,31 @@
+﻿package main
+
+import (
+	"github.com/highload/interfaces/app"
+	"github.com/highload/interfaces/handlers"
+	"log"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func main() {
+	log.Println("Server is preparing to start")
+	Application := app.GetApplication()
+
+	if Application.Config.Site.Disabled {
+		log.Println("Site is disabled")
+		Application.Routes(app.MapRoutes{"/": handlers.HandleDisabled{}})
+	} else {
+		Application.Routes(app.MapRoutes{
+			"/": handlers.HandleHome{},
+			"/v1/ajax/": handlers.HandleAjax{},
+			// другие контроллеры
+			"/{url:.*}": handlers.Handle404{},
+		})
+	}
+
+	Application.Run()
+	log.Println("Exit")
+}
